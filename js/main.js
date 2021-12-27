a3adc068db4809270aa0e633851268a98bd81aa2
mauricio sandoval <flashpoint_84@hotmail.com>
2021-12-27 20:08:21 -0300

    Se escribe el algoritmo condicional

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-let entrada = prompt('Ingrese un número');
+//Se le pide al usuario que ingrese su nombre
+let nombre = prompt('Ingresá tu Nombre');
 
+//Se le pide al usuario que ingrese su edad
+let edad = prompt('Ingresá tu edad');
+
+//Se le suma un número
 let salida = Number(entrada) + 20;
 
+//Se da un mensaje con el resultado
 alert(`${salida} es la suma de ${entrada} + 20`);
