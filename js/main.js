e630b53fe69d63eabf799e198db21c07ba54d720
mauricio sandoval <flashpoint_84@hotmail.com>
2022-01-09 21:36:40 -0300

    Se generan los event listeners y las primeras funciones

--- js/main.js
+++ js/main.js
@@ -1,16 +1,130 @@
 
-
+//Solicita el nombre del jugador
 
 let playerName = prompt('!Bienvenido!, ingrese su nombre para continuar');
+console.log(playerName);
 
-function PlayerNameSet(){
-    document.getElementById("player-name").innerHTML = `Player 1: ${playerName}`;
+
+//Lo pinta en la interfaz - DESAFÍO CLASE 03: CREAR UN ALGORITMO UTILIZANDO UN CICLO
+function playerNameSet(name){
+  while(name == "" || name== null){
+    name = prompt('Por favor ingrese su nombre para continuar');
+  }
+  document.getElementById("player-name").innerHTML = `Player 1: ${name}`;
 }
 
+playerNameSet(playerName);
 
+
+
+//Elije la direccion del disparo
+
+let ul = document.getElementById('ul');
+let uc = document.getElementById('uc');
+let ur = document.getElementById('ur');
+
+let ml = document.getElementById('ml');
+let mc = document.getElementById('mc');
+let mr = document.getElementById('mr');
+
+let dl = document.getElementById('dl');
+let dc = document.getElementById('dc');
+let dr = document.getElementById('dr');
+
+
+
+let getDirection;
+
+ul.addEventListener('click', ()=>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+uc.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+ur.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+ml.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+mc.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+mr.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+dl.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+dc.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+dr.addEventListener('click', () =>{
+  getDirection = Math.floor(Math.random() * 10);
+  if(getDirection<1){
+    getDirection+=1;
+  }
+  console.log(getDirection);
+});
+
+
+
+//Dispara
 let buttonShot = document.getElementById('shot');
-buttonShot.addEventListener('click', ()=>{
-    console.log('Disparo!');
-} );
 
-console.log(playerName);
+buttonShot.addEventListener('click', ()=>{
+  if(getDirection){
+    if(getDirection >= 5){
+      getDirection - Math.floor(Math.random() * 10)
+      console.log(getDirection);
+      alert('GOOOOL!!!')
+    }
+    else(
+      alert('ATAJÓ EL ARQUERO!!')
+    );
+  }
+  else{
+    alert('Por favor selecciona la dirección del disparo');
+  }
+  
+} );
