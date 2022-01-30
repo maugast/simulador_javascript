2780e50e8f9faaab88d5581c22b8ffa9ace641cc
mauricio sandoval <flashpoint_84@hotmail.com>
2022-01-30 12:20:57 -0300

    Se hacen ajustes en la interfaz

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 
-//Solicita el nombre del jugador
+//Variables
 
-let playerName = prompt('!Bienvenido!, ingrese su nombre para continuar');
-console.log(playerName);
-
-
-//Elije jugador
-
-//Se crean objetos a partir de la clase Avatar
+const playerName = document.querySelector('#player-name-input');
+const goButton = document.querySelector('#go-btn');
 const avatar1 = new Avatar("Lionel Messi","Paris SG","IZQ", 6,9);
 const avatar2 = new Avatar("Cristiano Ronaldo","Manchester Utd","DER", 7,8);
 const avatar3 = new Avatar("Robert Lewandowski","Bayern Munich","DER", 7,7);
@@ -19,9 +14,7 @@ const avatar8 = new Avatar("Gareth Bale","Real Madrid","IZQ", 6,8);
 const avatar9 = new Avatar("Neymar","Paris SG","DER", 6,7);
 const avatar10 = new Avatar("Antoine Griezmann","Atlético de Madrid","IZQ", 7,5);
 
-
-//Array de Objetos
-const listaAvatares = [
+const avatarList = [
         avatar1,
         avatar2,
         avatar3,
@@ -34,21 +27,32 @@ const listaAvatares = [
         avatar10
 ];
 
-
 const selectScreen = document.querySelector('.select-screen-container');
-const tablaAvatars = document.querySelector('.tabla-avatars');
+const avatarTable = document.querySelector('.avatar-table');
 const tdAvatar = document.getElementsByTagName('td');
+const allButton = document.getElementById('filter-all');
+const filterLeft = document.querySelector('#filter-l');
+const filterRight = document.querySelector('#filter-r');
+const avatarShow = document.querySelector('.avatar-show-static');
+const buttonShot = document.getElementById('shot');
 
 console.log(tdAvatar);
 
-//Pinta el nombre ingreado por prompt() en la interfaz
+
+//Event Listeners
+
+
+
+
+//Funciones
+
 function playerNameSet(name){
   while(name == "" || name == null){
-    name = prompt('Por favor ingrese su nombre para continuar');
+    name = playerName;
   }
   document.getElementById("player-name").innerHTML = `Player 1: ${name}`;
 
-  tablaAvatars.innerHTML = `
+  avatarTable.innerHTML = `
   <tbody>
       <tr>
           <td class="izq messi">${avatar1.nombre}</td>
@@ -75,21 +79,21 @@ function playerNameSet(name){
               `; 
 
   // Para revisión interna de datos
-  for(let jugador of listaAvatares){
+  for(let jugador of avatarList){
     console.log(jugador.nombre);
   }
 
 }
 
-playerNameSet(playerName);
+
 
 //Botones para filtrar resultados
 
 //Ver todos
-const allButton = document.getElementById('filter-all');
+
 
 allButton.addEventListener('click', () =>{
-  tablaAvatars.innerHTML = `
+  avatarTable.innerHTML = `
           <tbody>
               <tr>
                   <td class="izq messi">${avatar1.nombre}</td>
@@ -119,12 +123,12 @@ allButton.addEventListener('click', () =>{
 
 
 //Filtrar Perfiles Izquierdos
-const filterLeft = document.querySelector('#filter-l');
+
 
 filterLeft.addEventListener('click', ()=>{
-  let onlyLeft = listaAvatares.filter(avatar => avatar.perfil.includes("IZQ"));
+  let onlyLeft = avatarList.filter(avatar => avatar.perfil.includes("IZQ"));
   
-  tablaAvatars.innerHTML = `
+  avatarTable.innerHTML = `
           <tbody>
               <tr>
                   <td class="izq messi">${avatar1.nombre}</td>
@@ -149,12 +153,11 @@ filterLeft.addEventListener('click', ()=>{
 
 
 //Filtrar Perfiles Derechos
-const filterRight = document.querySelector('#filter-r');
 
 filterRight.addEventListener('click', ()=>{
-  let onlyRight = listaAvatares.filter(avatar => avatar.perfil.includes("DER"));
+  let onlyRight = avatarList.filter(avatar => avatar.perfil.includes("DER"));
 
-  tablaAvatars.innerHTML = `
+  avatarTable.innerHTML = `
           <tbody>
               <tr>
                   <td class="der cronaldo">${avatar2.nombre}</td>
@@ -178,21 +181,21 @@ filterRight.addEventListener('click', ()=>{
   console.log(onlyRight);
 });
 
-tablaAvatars.addEventListener('mouseover', showAvatar);
-tablaAvatars.addEventListener('mouseout', resetAvatar);
-tablaAvatars.addEventListener('click', chooseAvatar);
+avatarTable.addEventListener('mouseover', showAvatar);
+avatarTable.addEventListener('mouseout', resetAvatar);
+avatarTable.addEventListener('click', chooseAvatar);
 
 function chooseAvatar(e){
   e.preventDefault();
   
   let avatarName = e.target.innerText;
   
-  for(let jugador of listaAvatares){
-    let avatarData = jugador.nombre;
+  for(let avatar of avatarList){
+    let avatarData = avatar.nombre;
     if(avatarName == avatarData){
       console.log(`${avatarName} - ${avatarData}`);
 
-      let avatarPlaceholder = jugador.nombreYClub()
+      let avatarPlaceholder = avatar.nombreYClub()
       document.getElementById("avatar-name").innerHTML = avatarPlaceholder;
 
       break;
@@ -200,8 +203,6 @@ function chooseAvatar(e){
   }
   selectScreen.remove();
 }
-
-let avatarShow = document.querySelector('.avatar-show-static');
 
 function showAvatar(e){
   e.preventDefault();
@@ -266,8 +267,8 @@ let dc = document.getElementById('dc');
 let dr = document.getElementById('dr');
 
 
-const arco = document.querySelector('.tabla-arco');
-console.log(arco);
+const goal = document.querySelector('.goal-table');
+console.log(goal);
 let getDirection;
 
 
@@ -351,7 +352,6 @@ dr.addEventListener('click', () =>{
 
 
 //Dispara
-let buttonShot = document.getElementById('shot');
 
 buttonShot.addEventListener('click', ()=>{
   if(getDirection){
